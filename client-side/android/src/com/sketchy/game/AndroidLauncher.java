7f4b98d629132b7065aadd310deba8c017bb6720
michoy <michael.hoyer3@gmail.com>
2018-03-08 16:49:48 +0100

    setup

---
+++ client-side/android/src/com/sketchy/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sketchy.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sketchy.game.SketchyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SketchyGame(), config);
+	}
+}
---
+++ client-side/desktop/src/com/sketchy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sketchy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sketchy.game.SketchyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SketchyGame(), config);
+	}
+}
