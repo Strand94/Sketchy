8ec758740ae9b5b3b9ec84065f4cdda402b44969
Lars Egil <lek@kjenstad.no>
2018-03-21 20:54:38 +0100

    Super crude menu

--- client-side/core/src/com/sketchy/game/Views/DrawView.java
+++ client-side/core/src/com/sketchy/game/Views/DrawView.java
@@ -1,6 +1,81 @@
 package com.sketchy.game.Views;
 
-public class DrawView extends View {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.sketchy.game.SketchyGame;
+
+public class DrawView implements Screen {
+
+    final SketchyGame game;
+
+    private OrthographicCamera camera;
+
+    public DrawView(SketchyGame game){
+        this.game = game;
+
+        this.loadAssets();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
+    }
+
+    @Override
+    public void show(){
+
+    }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0.2f, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Draw View", 20, 550);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void pause(){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume(){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void hide(){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose(){
+
+    }
+
+    private void loadAssets(){
+
+    }
+
+    private void initializeEntities(){
+
+    }
+
     public void getDrawing() {
 
     }
--- client-side/core/src/com/sketchy/game/Views/LoginView.java
+++ client-side/core/src/com/sketchy/game/Views/LoginView.java
@@ -1,5 +1,77 @@
 package com.sketchy.game.Views;
 
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.sketchy.game.SketchyGame;
 
-public class LoginView extends View {
+public class LoginView implements Screen {
+    final SketchyGame game;
+
+    private OrthographicCamera camera;
+
+    public LoginView(SketchyGame game){
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
+    }
+
+    @Override
+    public void show(){
+
+    }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Main Menu", 20, 550);
+        game.font.draw(game.batch, "Sketchy", 250, 550);
+        game.font.draw(game.batch, "Click to play!", 20, 525);
+        game.batch.end();
+
+        if(Gdx.input.isTouched()){
+            System.out.print("Test");
+            game.setScreen(new DrawView(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void pause(){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume(){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void hide(){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose(){
+        // TODO Auto-generated method stub
+
+    }
+
 }
