4ca06c8e5d0a12898e2d8f40ccc13acf1eb898c6
Lars Egil <lek@kjenstad.no>
2018-03-22 10:18:31 +0100

    Crude drawing-feature implemented

--- client-side/core/src/com/sketchy/game/Views/DrawView.java
+++ client-side/core/src/com/sketchy/game/Views/DrawView.java
@@ -1,21 +1,58 @@
 package com.sketchy.game.Views;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.sketchy.game.SketchyGame;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 
+import java.util.Stack;
 
 public class DrawView extends View {
 
     final SketchyGame game;
     private OrthographicCamera camera;
 
-    //Text
-    private String draw = "Draw View";
-    private GlyphLayout draw_layout;
+    // Rendering
+    private ShapeRenderer shapeRenderer;
 
+    //Text
+    private String draw = "Draw View", guessWord;
+    private GlyphLayout draw_layout;
+    private GlyphLayout guessWord_layout;
+
+    private Stack<Dots> drawing;
+    private float current_radius = 5.0f;
+    private Color current_color = Color.CYAN;
+
+    /**
+     * A drawing consists of a stack of colored circles (Dots)
+     */
+    public class Dots {
+        private float radius;
+        Vector2 position;
+        Color color;
+
+        /**
+         * Dot constructor. Remember to add it to your Stack of Dots
+         * @param radius Dot size
+         * @param position Remember to render at (x, screenHeight - y))
+         * @param color gdx.Graphics.Color
+         */
+        public Dots(float radius, Vector2 position, Color color) {
+            this.radius = radius;
+            this.position = position;
+            this.color = color;
+        }
+    }
+
+    /**
+     *
+     * @param game
+     */
     public DrawView(SketchyGame game){
         this.game = game;
 
@@ -26,6 +63,40 @@ public class DrawView extends View {
 
         //Can be used to get font width (or height)
         draw_layout = new GlyphLayout(game.font, draw);
+
+        drawing = new Stack<Dots>();
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    /**
+     * Add colored Circle (Dot) at position of user input
+     * Drawing consists of lots of Dots.
+     */
+    private void Draw(){
+        if(Gdx.input.isTouched()){
+            float x = Gdx.input.getX();
+            float y = Gdx.input.getY();
+            drawing.add(new Dots(current_radius, new Vector2(x, y), current_color));
+        }
+    }
+
+    /**
+     * Render ALL the Dots!
+     */
+    private void RenderDrawing(){
+        for (Dots dot : drawing) {
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.setColor(dot.color);
+            shapeRenderer.circle(dot.position.x, screenHeight - dot.position.y, dot.radius);
+            shapeRenderer.end();
+        }
+    }
+
+    /**
+     * Renders ALL the text!
+     */
+    private void RenderText(){
+        game.font.draw(game.batch, draw, (screenWidth-draw_layout.width)*0.5f, screenHeight * (1-0.1f));
     }
 
     @Override
@@ -41,49 +112,30 @@ public class DrawView extends View {
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
+        Draw();
+
         game.batch.begin();
-        game.font.draw(game.batch, draw, (screenWidth-draw_layout.width)*0.5f, screenHeight * (1-0.1f));
+        RenderDrawing();
+        RenderText();
         game.batch.end();
     }
 
     @Override
-    public void resize(int width, int height){
-        // TODO Auto-generated method stub
-
-    }
+    public void resize(int width, int height){}
 
     @Override
-    public void pause(){
-        // TODO Auto-generated method stub
-
-    }
+    public void pause(){}
 
     @Override
-    public void resume(){
-        // TODO Auto-generated method stub
-
-    }
+    public void resume(){}
 
     @Override
-    public void hide(){
-        // TODO Auto-generated method stub
-
-    }
+    public void hide(){}
 
     @Override
-    public void dispose(){
+    public void dispose(){}
 
-    }
+    private void loadAssets(){}
 
-    private void loadAssets(){
-
-    }
-
-    private void initializeEntities(){
-
-    }
-
-    public void getDrawing() {
-
-    }
+    public void getDrawing() {} // TODO
 }
