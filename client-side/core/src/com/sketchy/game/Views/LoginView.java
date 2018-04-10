bb6b0bf2599c13d4e7e1aec71031f9616224c059
Lars Egil <lek@kjenstad.no>
2018-04-10 10:04:39 +0200

    Move initialization to parent class

--- client-side/core/src/com/sketchy/game/Views/LoginView.java
+++ client-side/core/src/com/sketchy/game/Views/LoginView.java
@@ -1,42 +1,18 @@
 package com.sketchy.game.Views;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.sketchy.game.SketchyGame;
 
 public class LoginView extends View{
     final SketchyGame game;
 
-    Table table;
-    Stage stage;
-
     public LoginView(SketchyGame game){
         this.game = game;
-
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
-
-        // Table for keeping track of position of UI elements
-        table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
-
-        // UI skin
-        Skin uiSkin = new Skin(Gdx.files.internal("uiSkin.json"));
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("uiSkin.atlas"));
-        uiSkin.addRegions(atlas);
 
         // Header
         Label header = new Label("Sketchy", uiSkin);
@@ -77,8 +53,6 @@ public class LoginView extends View{
                 System.out.println("Create Game");
             }
         });
-
-
     }
 
     @Override
@@ -88,26 +62,18 @@ public class LoginView extends View{
 
     @Override
     public void render(float delta){
+        super.render(delta);
         Gdx.gl.glClearColor(68.0f/256, 117.0f/256, 180.0f/256, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.act(delta);
-        stage.draw();
-
     }
 
     @Override
     public void resize(int width, int height) {
-
-        //resize cam viewport
-        stage.getCamera().viewportWidth = Gdx.graphics.getWidth();
-        stage.getCamera().viewportHeight = Gdx.graphics.getHeight();
-
+        super.resize(width, height);
     }
 
     @Override
     public void dispose(){
-        stage.dispose();
+        super.dispose();
     }
 
 }
--- client-side/core/src/com/sketchy/game/Views/View.java
+++ client-side/core/src/com/sketchy/game/Views/View.java
@@ -2,12 +2,36 @@ package com.sketchy.game.Views;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class View implements Screen{
 
     final int screenWidth = Gdx.graphics.getWidth();
     final int screenHeight = Gdx.graphics.getHeight();
+
+    Stage stage;
+    Table table;
+    Skin uiSkin;
+
+    public View() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        // UI skin
+        uiSkin = new Skin(Gdx.files.internal("uiSkin.json"));
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("uiSkin.atlas"));
+        uiSkin.addRegions(atlas);
+
+        // Table for keeping track of position of UI elements
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+    }
 
     @Override
     public void show(){
@@ -16,13 +40,17 @@ public class View implements Screen{
 
     @Override
     public void render(float delta){
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
 
     }
 
     @Override
     public void resize(int width, int height){
-        // TODO Auto-generated method stub
-
+        stage.getCamera().viewportWidth = Gdx.graphics.getWidth();
+        stage.getCamera().viewportHeight = Gdx.graphics.getHeight();
     }
 
     @Override
@@ -45,8 +73,7 @@ public class View implements Screen{
 
     @Override
     public void dispose(){
-        // TODO Auto-generated method stub
-
+        stage.dispose();
     }
 
 }
