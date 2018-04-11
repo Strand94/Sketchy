de03bf6cee47e492cf94a67f3f09c38ffd3dae52
michoy <michael.hoyer3@gmail.com>
2018-04-11 21:55:41 +0200

    adds express

--- server-side/communicator.js
+++ server-side/communicator.js
@@ -1,4 +1,6 @@
-const io = require("socket.io");
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
 
 class Communicator {
     constructor(gameController, lobbyController) {
@@ -10,5 +12,14 @@ class Communicator {
     beginRound(playerAddress, sheet) { }
     getAnswer(playerAddress) { }
 }
+
+server.listen(8080, function(){
+	console.log("Server is now running...");
+});
+
+io.on('connection', function(socket) {
+    console.log("Player Connected!");
+	socket.emit('socketID', { id: socket.id });
+});
 
 module.exports = Communicator;
