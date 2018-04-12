20bd9b9cb39a0f8dbd36c9afbc2f83225fd9f4e9
Lars Egil Kjenstad <lek@kjenstad.no>
2018-04-12 13:09:30 +0200

    Add Player constructor

--- client-side/core/src/com/sketchy/game/Models/Player.java
+++ client-side/core/src/com/sketchy/game/Models/Player.java
@@ -3,7 +3,11 @@ package com.sketchy.game.Models;
 public class Player {
     String name;
     String clientAdress;
-    int points;
+    int points = 0;
+
+    public Player(String name) {
+        this.name = name;
+    }
 
     public void addPoints(int points){
 
