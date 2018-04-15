Strand94/Sketchy

commit 81b7cd4cc583b52ee5e28e0663ca05b39d509a53

gives clientController a game and communicator

client-side/core/src/com/sketchy/game/Controllers/ClientController.java
@@ -3,20 +3,23 @@ package com.sketchy.game.Controllers;
 import com.sketchy.game.Models.Player;
 import com.sketchy.game.SketchyGame;
 import com.sketchy.game.Views.LobbyView;
-import com.sketchy.game.Views.LoginView;
 import com.sketchy.game.Views.View;
 import com.sketchy.game.Models.Sheet;
+import com.sketchy.game.communicator.Communicator;
 
 import java.util.List;
 
 public class ClientController {
 
-    SketchyGame game;
-    Player player;
-    public View view;
+    private SketchyGame game;
+    private Player player;
+    private Communicator communicator;
+    private View view;
+
 
     public ClientController(SketchyGame game) {
         this.game = game;
+        this.communicator = new Communicator(this);
     }
 
     public void startGame() {
@@ -58,8 +61,23 @@ public class ClientController {
         game.setScreen(view);
         this.view.dispose();
         this.view = view;
-        System.out.println("SetView:" + game.clientController.view);
+        System.out.println("SetView:" + view);
 
     }
 
+    public SketchyGame getGame() {
+        return game;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Communicator getCommunicator() {
+        return communicator;
+    }
+
+    public View getView() {
+        return view;
+    }
 }

client-side/core/src/com/sketchy/game/SketchyGame.java
@@ -11,8 +11,8 @@ public class SketchyGame extends Game {
 
     public SpriteBatch batch;
     public BitmapFont font;
-    public Communicator communicator;
-    public ClientController clientController;
+    private Communicator communicator;
+    private ClientController clientController;
 
     public int MAX_PLAYERS = 8;
 
@@ -20,12 +20,12 @@ public class SketchyGame extends Game {
     public void create() {
         font = new BitmapFont(); // Use default Arial font
         batch = new SpriteBatch();
-        communicator = new Communicator(this);
         clientController = new ClientController(this);
+
 
         LoginView loginView = new LoginView(this);
         this.setScreen(loginView);
-        clientController.view = loginView;
+        clientController.setView(loginView);
 
     }
 

client-side/core/src/com/sketchy/game/communicator/Communicator.java
@@ -3,7 +3,6 @@ package com.sketchy.game.communicator;
 import com.sketchy.game.Config;
 import com.sketchy.game.Controllers.ClientController;
 import com.sketchy.game.Models.Sheet;
-import com.sketchy.game.SketchyGame;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,16 +20,18 @@ import static com.sketchy.game.communicator.Event.*;
 public class Communicator {
     private Socket socket;
     private HashMap<Event, Listener> serverEventMap;
-    SketchyGame game;
+    private ClientController clientController;
 
-    public Communicator(SketchyGame game) {
+    public Communicator(ClientController clientController) {
         connect(false);
         populateServerEventMap();
+        this.clientController = clientController;
     }
 
-    public Communicator(boolean local, SketchyGame game) {
+    public Communicator(ClientController clientController, boolean local) {
         connect(local);
         populateServerEventMap();
+        this.clientController = clientController;
     }
 
     private void connect(boolean local) {
@@ -116,23 +117,23 @@ public class Communicator {
     }
 
     private void onStartGame() {
-        game.clientController.startGame();
+        clientController.startGame();
     }
 
     private void onEndGame() {
-        game.clientController.endGame();
+        clientController.endGame();
     }
 
     private void onUpdateView() {
-        game.clientController.updateView();
+        clientController.updateView();
     }
 
     private void onUpdateLobby(List<String> members) {
-        game.clientController.updateLobby(members);
+        clientController.updateLobby(members);
     }
 
     private void onBeginRound(Sheet sheet) {
-        game.clientController.beginRound(sheet);
+        clientController.beginRound(sheet);
     }
 
     private void onGetAnswer() {

client-side/desktop/src/com/sketchy/game/desktop/DesktopLauncher.java
@@ -16,13 +16,5 @@ public class DesktopLauncher {
 		config.width = 720/2;
 		config.height = 1280/2;
 
-		try {
-			Communicator communicator = new Communicator(false, game);
-			communicator.joinLobby(1337, "hans gregor");
-			TimeUnit.SECONDS.sleep(5);
-		} catch (Exception e) {
-			System.out.println("Can't connect to server");
-		}
-
 	}
 }

commit fe9e4b989e08f8cc874c6b003b51cd582395d670

Fix null-ref error introduced by 81b7cd4cc583b52ee5e28e0663ca05b39d509a53

client-side/core/src/com/sketchy/game/Controllers/ClientController.java
@@ -59,8 +59,12 @@ public class ClientController {
 
     public void setView(View view){
         game.setScreen(view);
-        this.view.dispose();
+        
+        if (this.view != null) {
+            this.view.dispose();
+        }
         this.view = view;
+
         System.out.println("SetView:" + view);
 
     }